db60de7403b4dd22e71dcb04a97c2ef22a44f2da
Taylor Cool <taycool@gmail.com>
2020-01-16 13:07:12 -0800

    configured for deployment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
+require('dotenv').config();
 const server = require('./server.js');
+const port = process.env.PORT || 4000;
 
 server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
+  console.log(`* Server Running on http://localhost:${port} *`);
 });
