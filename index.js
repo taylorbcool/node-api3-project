1310e41fcd3046c2db329b1275b8d74bbec3ee92
Taylor Cool <taycool@gmail.com>
2020-01-16 13:17:27 -0800

    PORT stuff

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 const server = require('./server.js');
 const port = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+server.listen(process.env.PORT || 4000, () => {
   console.log(`* Server Running on http://localhost:${port} *`);
 });
